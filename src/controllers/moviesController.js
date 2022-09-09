812092c644c797b1986529632d5c287b11c7c6c4
UliaKarpova <chelovek-e-95@mail.ru>
2022-09-09 10:10:48 +0300

    test addMovies

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -8,10 +8,10 @@ const notFoundErrorMessage = 'Карточка не найдена';
 const forbiddenDeleteCardErrorMessage = 'Можно удалять только свою карточку';
 
 module.exports.createMovie = (req, res, next) => {
-  const { country, director, duration, year, description, image, trailer, nameRU, nameEN, thumbnail, movieId } = req.body;
+  const { country, director, duration, year, description, image, trailer, nameRU, nameEN, thumbnail, movieId, owner } = req.body;
   console.log(req.body);
   Movie.create({
-    country, director, duration, year, description, image, trailer, nameRU, nameEN, thumbnail, movieId, owner: req.user._id
+    country, director, duration, year, description, image, trailer, nameRU, nameEN, thumbnail, movieId, owner/*: req.user._id*/
   })
     .then((movie) => {
       res.status(200).send({ movie }).end();
